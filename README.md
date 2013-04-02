4fad98959d7e1ebb2fff8ecabdf3ec6c807d1d9f
mbcreation <benoit@mbcreation.net>
2013-04-02 17:12:46 +0200

    Initial commit
    Under developpement !

---
+++ README.md
@@ -0,0 +1,4 @@
+CacheMachine
+================
+
+Php and htaccess scripts to cache entire WordPress GET request in flat html files. 
---
+++ cache.php
@@ -0,0 +1,41 @@
+<?php
+
+
+if($_SERVER['REMOTE_ADDR'] != 'IP SERVEUR' && $_SERVER['REQUEST_METHOD'] == 'GET' && empty($_GET) && null === $cookie = $_COOKIE['wordpress_logged_in_'.md5('http://'.$_SERVER['HTTP_HOST'])]){
+
+
+	$cachefolder = "cache";
+	
+	$flaturl = end(explode('/',rtrim($_SERVER['REQUEST_URI'],'/')));
+	
+	if($flaturl == '') $flaturl = 'home';
+
+		if(!file_exists($cachefolder.'/'.$flaturl)){
+          
+		$data = file_get_contents('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+		$response = $http_response_header[0];
+		if(strpos($response, '200')!==false){
+			
+			file_put_contents($cachefolder.'/'.$flaturl, $data);
+		}
+		else{
+			
+			include('index.php');
+			exit;
+
+		}
+
+
+	}
+	
+	readfile($cachefolder.'/'.$flaturl);
+	exit;
+
+}
+
+else{
+	
+	include('index.php');
+	exit;
+
+}
